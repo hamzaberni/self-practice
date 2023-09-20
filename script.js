1c3857b7149820aa3de51ff602e2a1d64fc558bc
hamzaberni <hamzaberni@gmail.com>
2023-09-20 17:33:12 +0200

    self-practice function

---
+++ script.js
@@ -0,0 +1,17 @@
+function logger (somethingNew) {
+
+    console.log (somethingNew)
+}
+
+
+function theDivOfTwoNumbers (x , y) {
+
+    console.log(y / x);
+
+    let sum = -5;
+    sum = y / x;
+
+    return sum;
+}
+
+logger (theDivOfTwoNumbers(5 , 10))
